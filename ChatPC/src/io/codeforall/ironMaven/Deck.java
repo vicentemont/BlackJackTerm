30a27b3da37ab98274fc48c56382ed8da5d0dce2
XixoEcaP <159262212+XixoEcaP@users.noreply.github.com>
2024-03-01 14:23:40 +0000

    Add files via upload

--- ChatPC/src/io/codeforall/ironMaven/Deck.java
+++ ChatPC/src/io/codeforall/ironMaven/Deck.java
@@ -1,20 +1,43 @@
 package io.codeforall.ironMaven;
 
-public class Deck {
-    Card[] cardDeck = new Card[52];
-    Suit[] suits = {Suit.DIAMONDS,Suit.CLUBS,Suit.HEARTS,Suit.SPADES};
+import java.util.Collections;
+import java.util.LinkedList;
 
+import java.util.LinkedList;
+
+public class Deck {
+    private LinkedList<Card> cardDeck;
+    Suit[] suits = {Suit.DIAMONDS, Suit.CLUBS, Suit.HEARTS, Suit.SPADES};
 
     public Deck() {
+        cardDeck = new LinkedList<>();
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 13; j++) {
-
-                Card card = cardDeck[i + (i + 1) * j];
-                card = new Card(suits[i],j);
+                Card card = new Card(suits[i], j);
+                cardDeck.add(card);
                 System.out.println(card);
-
             }
+        }
 
+
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cardDeck);
+    }
+
+    public Card dealCard() {
+        if (!cardDeck.isEmpty()) {
+            return cardDeck.removeFirst();
+        } else {
+            System.out.println("Deck is empty.");
+            return null;
         }
     }
+
+    public static void main(String[] args) {
+        new Deck();
+    }
 }
+
+
